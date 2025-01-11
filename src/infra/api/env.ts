a5ae8b36f366e56748c82f121ab9a6c37d116da0
RafaelSSantiago <raphasantiago768@gmail.com>
2025-01-11 20:57:32 -0300

    feat: add docker

--- src/infra/api/env.ts
+++ src/infra/api/env.ts
@@ -1,3 +1,3 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || "mongodb://localhost:27017/todo",
+  mongoUrl: process.env.MONGO_URL || "mongodb://admin:secret@mongodb:27017/todo?authSource=admin",
 };
